354c6452232f062854871a5ffbca4a2db3a26279
TCG-Akash-Murumkar <akash@thegamificationcompany.com>
2025-02-18 15:53:14 +0530

    code

---
+++ skillmuniwebservice/Models/AssesmentJSONResponce.cs
@@ -0,0 +1,11 @@
+using m2ostnextservice.Models;
+using System.Collections.Generic;
+
+public class AssesmentJSONResponce
+{
+    public List<BriefChart> right;
+    public List<BriefChart> wrong;
+    public UserScoreResponse scoreres;
+    public BriefResource briefDatals;
+
+}
